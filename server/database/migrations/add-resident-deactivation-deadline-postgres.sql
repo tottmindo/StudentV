5e4ad2006a20f1c7a6e57a92a3975d6e2cc405ed
Laitila93 <laitila93@gmail.com>
2026-08-24 16:31:20 +0200

    refactor: remove obsolete database migration scripts and update address constraint in dorms table

--- server/database/migrations/add-resident-deactivation-deadline-postgres.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE users
-  ADD COLUMN IF NOT EXISTS scheduleddeactivationat timestamptz;
-
-CREATE INDEX IF NOT EXISTS idx_users_scheduled_deactivation
-  ON users (scheduleddeactivationat)
-  WHERE active = true AND scheduleddeactivationat IS NOT NULL;
--- server/database/migrations/normalize-house-numbers-postgres.sql
+++
@@ -1,9 +0,0 @@
--- Store only the numeric house identifier. Presentation such as "House" or
--- "Hus" belongs to the localized client.
-UPDATE dorms
-SET address = substring(address FROM '[0-9]+')
-WHERE address !~ '^[0-9]+$'
-  AND address ~ '[0-9]+';
-
-ALTER TABLE dorms DROP CONSTRAINT IF EXISTS dorms_address_number_check;
-ALTER TABLE dorms ADD CONSTRAINT dorms_address_number_check CHECK (address ~ '^[0-9]+$');
